8149778f497834aa06136064e75d532cef296266
Louis Chanouha <louis@chanouha.fr>
2016-08-20 01:02:26 +0200

    Bug: whe shouldn't be redirected while loggin in

--- app/assets/javascripts/casino/sessions.js
+++ app/assets/javascripts/casino/sessions.js
@@ -1,4 +1,5 @@
 (function(win, doc) {
+  var timeoutAutoLogin;
   var url = win.CASino.url('login'),
       cookie_regex = /(^|;)\s*tgt=/,
       ready_bound = false;
@@ -13,18 +14,23 @@
       }
       win.location = url;
     } else {
-      setTimeout(checkCookieExists, 1000);
+      timeoutAutoLogin = setTimeout(checkCookieExists, 1000);
     }
   }
 
   // Auto-login when logged-in in other browser window (9887c4e)
+  // @lchanouha fix, 08/2016: whe should'nt be redirected while loggin in
   doc.addEventListener('DOMContentLoaded', function() {
     if(ready_bound) {
       return;
     }
     ready_bound = true;
-    if(doc.getElementById('login-form')) {
+    var loginFrom = doc.getElementById('login-form')
+    if(loginFrom) {
       checkCookieExists();
+      loginFrom.addEventListener("submit", function(){
+        clearTimeout(timeoutAutoLogin);
+      });
     }
   });
 
